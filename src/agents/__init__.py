18219f59800d6de17eafb8675324586e1abdae2a
Yuki Kimoto <103980824+yukikimoto@users.noreply.github.com>
2023-12-21 05:46:35 +0000

    mainAgentの呼び出し方の変更

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,1 +1,10 @@
+import main
 from . import tools
+
+if __name__ == "__main__":
+    # ユーザーの入力を受け取る
+    user_message = input("User >> ")
+    # ユーザーの入力を引数にして実行する
+    result = main.run(user_message)
+    # 結果を表示する
+    print("システム応答 : ", result)
