2ca1ea35d1f51efdc6ad330ccf89f2806ad264b8
Yuki Kimoto <103980824+yukikimoto@users.noreply.github.com>
2024-01-25 00:51:42 +0900

    Update version number in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def _from_file(file_name):
 
 setup(
     name='tech_agents',
-    version='0.1.0',
+    version='0.1.1',
     author='',
     description='tech_agents : agents use toolagent',
     packages=find_packages(),
