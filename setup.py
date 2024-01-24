d3af38daca383854f7d36e3422bf827670c8228d
Yuki Kimoto <103980824+yukikimoto@users.noreply.github.com>
2024-01-24 05:39:58 +0000

    Refactor setup.py to use relative paths and improve code readability

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ import sys
 sys.path.append('./src')
 # sys.path.append('./test')
 
-def requirements_from_file(file_name):
+def _from_file(file_name):
     return open(file_name).read().splitlines()
+
 
 setup(
     name='tech_agents',
     version='0.0.2',
     author='Yuki Kimoto',
-    packages=find_packages(),
-    install_requires=requirements_from_file('requirements.txt'),
-    license="LICENSE.txt",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=_from_file('requirements.txt'),
+    license=_from_file('LICENSE.txt'),
 )
