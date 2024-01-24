a556a22ea1680470ee994f604827a86b64c71a92
Yuki Kimoto <103980824+yukikimoto@users.noreply.github.com>
2024-01-24 07:01:31 +0000

    Delete unused files and update version number

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 from setuptools import setup, find_packages
-import sys
-sys.path.append('./src')
-# sys.path.append('./test')
 
 def _from_file(file_name):
     return open(file_name).read().splitlines()
 
-
 setup(
     name='tech_agents',
-    version='0.0.6',
+    version='0.0.7',
     author='Yuki Kimoto',
-    packages=find_packages("src"),
-    package_dir={"": "src"},
+    packages=find_packages(),
     install_requires=_from_file('requirements.txt'),
     license='LICENSE.txt',
 )
